a599c1ee3624d3cef02c0773ce16a8f816073bb3
Robin Mauritz <robinmauritzrm@gmail.com>
2025-04-08 19:14:59 +0000

    fix(playlist-getter): Empty print on single page result

--- playlist-getter.py
+++ playlist-getter.py
@@ -3,31 +3,24 @@ from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
 
-base_hostname = None
-fetch_url_base = None
+import typer
+from typing_extensions import Annotated
 
 def get_page_links(soup):
     return [link.get('href') for link in soup.find_all('a') if link.get('href').startswith("page") and link.get('href').endswith(".html")]
 
-def get_video_links(soup):
-    if not fetch_url_base:
-        raise Exception("Hostname not defined")
-
-    video_links = [link.get('href') for link in soup.find_all('a') if link.get('href').startswith(fetch_url_base)]
+def get_video_links(soup, starts_with: str):
+    video_links = [link.get('href') for link in soup.find_all('a') if link.get('href').startswith(starts_with)]
     return [urlparse(link)._replace(query=None).geturl() for link in video_links]
 
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def main(base_url):
-    global base_hostname, fetch_url_base
-
+def main(base_url: str, reverse: Annotated[bool, typer.Option()] = False):
     parsed_url = urlparse(base_url)
     base_hostname = parsed_url.netloc
     fetch_url_base = f"https://{base_hostname}/video/"
-
-    print()
 
     video_links = []
 
@@ -37,13 +30,11 @@ def main(base_url):
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    links = get_video_links(soup)
+    links = get_video_links(soup, fetch_url_base)
+
     page_links = get_page_links(soup)
     page_nums = [int(link.strip("page").strip(".html")) for link in page_links]
     max_page_num = max(page_nums, default=0)
-
-    if max_page_num == 0:
-        return links
 
     video_links += links
 
@@ -53,16 +44,13 @@ def main(base_url):
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
-        video_links += get_video_links(soup)
+        video_links += get_video_links(soup, fetch_url_base)
 
-    return video_links
+    if reverse:
+        video_links.reverse()
+
+    for link in video_links:
+        print(link)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python playlist-getter.py <base_url>")
-        sys.exit(1)
-    list = main(sys.argv[1])
-
-    # list.reverse()
-    for link in list:
-        print(link)
+    typer.run(main)
